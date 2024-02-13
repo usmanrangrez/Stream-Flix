29ccc336914207cb03a97beed5ff8e2c42e1936c
usmanrangrez <usmaanrangrez@gmail.com>
2024-02-13 23:34:55 +0530

    resposive image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,11 @@ import { TMDB_CDN_URL } from "../utils/constants";
 const MovieCard = ({ movie }) => {
   return (
     <div className="w-28 h-52 sm:w-48 sm:h-72 pr-3 sm:pr-4  ">
-      <img src={TMDB_CDN_URL + movie?.poster_path} alt="Movie_Poster" />
+      <img
+        className="rounded-sm sm:rounded-md"
+        src={TMDB_CDN_URL + movie?.poster_path}
+        alt="Movie_Poster"
+      />
     </div>
   );
 };
